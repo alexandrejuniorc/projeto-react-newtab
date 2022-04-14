3ca0f471274cab3f04f4fd645dedb0a492b44be2
Alexandre Junior <alexandrejunior408@gmail.com>
2022-04-14 16:57:42 -0300

    adiciona mascara do campo input

---
+++ src/Components/Mascara/Mascara.js
@@ -0,0 +1,12 @@
+export function Mascara(event) {
+  const val = event.target.value
+    .replace(/\D/g, '')
+    .replace(/^0*/, '')
+    .padStart(3, '0');
+
+  event.target.value =
+    'R$ ' +
+    val.slice(0, -2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.') +
+    ',' +
+    val.slice(-2);
+}
