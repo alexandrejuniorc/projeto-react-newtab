044152bc2f95ff929cfb2f6281a2adcb4d4dae7a
Alexandre Junior <alexandrejunior408@gmail.com>
2022-04-14 16:59:13 -0300

    update do componente de pagamento

--- src/Components/Modal/Pagamento/Pagamento.js
+++ src/Components/Modal/Pagamento/Pagamento.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './styles.scss';
-
+import { cartoes } from '../../Cartoes/cartoes';
+import { Mascara } from '../../Mascara/Mascara';
 const Pagamento = () => {
   return (
     <div className="bgPagamento">
       <div className="bgInput">
-        <input type="text" placeholder="R$ 0,00" />
+        <input
+          type="text"
+          placeholder="R$ 0,00"
+          onKeyUp={Mascara}
+          maxLength="30"
+          required
+        />
       </div>
 
       <div className="bgSelect">
-        <select name="" id="">
-          <option>Nº Cartão</option>
+        <select name="selecionaCartao">
+          <option disabled>Nº Cartão</option>,
+          {cartoes.map((cartao) => {
+            return (
+              <option key={cartao.card_number} value={cartao.card_number}>
+                Cartão com final {cartao.card_number.substring(12)}
+              </option>
+            );
+          })}
         </select>
       </div>
       <div className="botaoPagar">
