fff550f6899372c47afec0b6b56ab8f2763c1874
Alexandre Junior <alexandrejunior408@gmail.com>
2022-04-14 18:00:19 -0300

    update app e pagamento

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import Modal from './Components/Modal/Modal';
 import Pagamento from './Components/Modal/Pagamento/Pagamento';
 import Aprovado from './Components/Modal/Aprovado/Aprovado';
 import Recusado from './Components/Modal/Recusado/Recusado';
+import { Mascara } from './Components/Mascara/Mascara';
+import { cartoes } from './Components/Cartoes/cartoes';
 
 function App() {
   // Hooks
   const [usuario, setUsuario] = useState([]);
   const [modal, setModal] = useState(false);
   const [pagamento, setPagamento] = useState(null);
+  const [pagamentoSucesso, setSucesso] = useState(false);
+  const [pagamentoErro, setErro] = useState(false);
 
   //  requisição dos dados da API de usuários
   // foi usado o useEffect para que não tenha um loop infinito e seja chamada a API uma vez só
@@ -20,6 +24,40 @@ function App() {
       .then((data) => setUsuario(data))
       .catch((erro) => console.log(erro));
   }, []);
+
+  const dadosCC = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const value = formData.get('valorPago');
+    const cartao = formData.get('selecionaCartao');
+    const selecaoCartao = cartoes.find(
+      (cartaoObjeto) => cartaoObjeto.card_number === cartao,
+    );
+
+    await await fetch(
+      'https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989',
+      {
+        method: 'POST',
+        body: {
+          card_number: cartao,
+          cvv: selecaoCartao,
+          expiry_date: selecaoCartao.expiry_date,
+
+          destination_user_id: pagamento.id,
+
+          value: value,
+        },
+      },
+    ).json();
+
+    if (cartao === '1111111111111111') {
+      setSucesso(true);
+    } else {
+      setErro(true);
+    }
+    setModal(false);
+  };
 
   // função que irá abrir o modal
   // também irá seta o pagamento para o map 'usuario'
@@ -49,14 +87,56 @@ function App() {
         // Modal Aberto para fazer o pagamento
         modal ? (
           <Modal
-            titulo="Pagamento para"
+            titulo="Pagamento para "
             subtitulo={pagamento.name}
             fechar={() => setModal(false)}
           >
-            <Pagamento />
+            <Pagamento>
+              <div className="bgPagamento">
+                <form className="bgInput" onSubmit={dadosCC}>
+                  <input
+                    name="valorPago"
+                    type="text"
+                    placeholder="R$ 0,00"
+                    onKeyUp={Mascara}
+                    maxLength="30"
+                    required
+                  />
+                </form>
+
+                <div className="bgSelect">
+                  <select name="selecionaCartao">
+                    <option disabled>Nº Cartão</option>,
+                    {cartoes.map((cartao) => {
+                      return (
+                        <option
+                          key={cartao.card_number}
+                          value={cartao.card_number}
+                        >
+                          Cartão com final {cartao.card_number.substring(12)}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+                <div className="botaoPagar">
+                  <button type="submit">Pagar</button>
+                </div>
+              </div>
+            </Pagamento>
           </Modal>
         ) : null
       }
+      {pagamentoSucesso && (
+        <Modal>
+          <Aprovado />
+        </Modal>
+      )}
+      {pagamentoErro && (
+        <Modal>
+          <Recusado />
+        </Modal>
+      )}
     </>
   );
 }
--- src/Components/Modal/Pagamento/Pagamento.js
+++ src/Components/Modal/Pagamento/Pagamento.js
@@ -2,36 +2,8 @@ import React from 'react';
 import './styles.scss';
 import { cartoes } from '../../Cartoes/cartoes';
 import { Mascara } from '../../Mascara/Mascara';
-const Pagamento = () => {
-  return (
-    <div className="bgPagamento">
-      <div className="bgInput">
-        <input
-          type="text"
-          placeholder="R$ 0,00"
-          onKeyUp={Mascara}
-          maxLength="30"
-          required
-        />
-      </div>
-
-      <div className="bgSelect">
-        <select name="selecionaCartao">
-          <option disabled>Nº Cartão</option>,
-          {cartoes.map((cartao) => {
-            return (
-              <option key={cartao.card_number} value={cartao.card_number}>
-                Cartão com final {cartao.card_number.substring(12)}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div className="botaoPagar">
-        <button type="submit">Pagar</button>
-      </div>
-    </div>
-  );
+const Pagamento = ({ children }) => {
+  return <div>{children}</div>;
 };
 
 export default Pagamento;
